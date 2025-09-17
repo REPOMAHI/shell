fe2771466f626d1945559888cf4b4b44c872852e
REPOMAHI <maharshisunkara2000@gmail.com>
2025-09-17 13:51:13 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -6,11 +6,19 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOG_FILE="/iyer"
+echo " iyer directory is not there "
+mkdir -p $LOG_FILE
+echo " iyer directory cretaed "
+
 dnf install nodejs -y
 if [ $? -ne 0 ]
 then
-   echo -e " niginx $R not installed $N "
+   echo -e " niginx $R not installed $N " &>> $LOG_FILE
 else
    echo -e " $G niginx installed $N "
 fi
+
+
+
 
